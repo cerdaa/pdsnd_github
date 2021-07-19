560e703642c74cf8928548683e6c57f38d8e2975
Cerda <andres.cerda@mckesson.com>
2021-07-19 14:10:37 -0500

    Refactor: Remove reporting processing times

--- bikeshare.py
+++ bikeshare.py
@@ -198,7 +198,6 @@ def time_stats(df):
     print('The most common start hour is: ' + str(df['start_hour'].value_counts().idxmax()) + ':00')
     print('The number of incidences at that hour is: '+ str(df['start_hour'].value_counts().max()))
 
-    print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
 
     return display_row_data(df)
@@ -227,7 +226,6 @@ def station_stats(df):
     print('The most frequent combination of start and end station is: ' + str(df.groupby(['Start Station'])['End Station'].value_counts().idxmax()))
     print('The number of incidences of such combination is: ' + str(df.groupby(['Start Station'])['End Station'].value_counts().max()))
 
-    print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
 
     return display_row_data(df)
@@ -255,7 +253,6 @@ def trip_duration_stats(df):
     # TO DO: display mean travel time
     print('The mean travel time is: ' + "{:.2f}".format(df['Trip Duration'].mean()/60) + ' min')
 
-    print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
 
     return display_row_data(df)
@@ -294,7 +291,6 @@ def user_stats(df):
     except:
         print('This city does not have birthday information')
 
-    print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
     return display_row_data(df)
 
